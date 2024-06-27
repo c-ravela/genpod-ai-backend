a772a3fdd5f3a3cf0180c8341838202f841f95ce
VishwasSomasekhariah <vkumar@intelops.dev>
2024-06-27 16:41:40 -0500

    Adding more project constants.

--- configs/supervisor_config.py
+++ configs/supervisor_config.py
@@ -6,3 +6,10 @@ THREAD_IDS = {
     'Planner':'3',
     'Coder':'4'
 }
+
+# Currently only use this value for collection_name if you have embeded and saved vector into the db with a differnet name then you can use it here.
+VECTOR_DB_COLLECTIONS = {'MISMO-version-3.6-docs':"C:/Users/vkumar/Desktop/genpod-ai-backend/vector_collections"}
+
+MEMBERS = ['RAG','Architect','Planner']
+
+# VECTOR_DB_PERSISTENCE_LOCATION = "C:/Users/vkumar/Desktop/genpod-ai-backend/vector_collections"
