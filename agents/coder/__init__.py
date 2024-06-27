688cfa806a3afbf7f09154cd517ddf1642097210
VishwasSomasekhariah <vkumar@intelops.dev>
2024-06-27 11:12:21 -0500

    Adding __init__.py to architect and coder

--- agents/coder/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Coder Agent
-
-Computer Programmer responsible for finshing up the project development by taking inputs from architect.
-
-* Develop the project by following the steps of architect.
-"""
