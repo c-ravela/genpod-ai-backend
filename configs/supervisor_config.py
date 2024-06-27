593fb2ebaeb90c7c4adecade3f2499b0271d946e
VishwasSomasekhariah <vkumar@intelops.dev>
2024-06-27 13:53:18 -0500

    Adding thread_id's as configurable constants in supervisor_config.py

---
+++ configs/supervisor_config.py
@@ -0,0 +1,8 @@
+""" constants to use in the supervisor agent"""
+# thread ids are crucial for graph state persistence so there should be unique for each agent key.
+THREAD_IDS = {
+    'RAG':'1',
+    'Architect':'2',
+    'Planner':'3',
+    'Coder':'4'
+}
