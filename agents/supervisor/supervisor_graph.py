a0039c3d361d3198d356afe247a303e4fed19a08
VishwasSomasekhariah <vkumar@intelops.dev>
2024-06-28 16:25:18 -0500

    Update supervisor graph to integrate with other agents

--- agents/supervisor/supervisor_graph.py
+++ agents/supervisor/supervisor_graph.py
@@ -1,34 +1,58 @@
 from langgraph.graph import StateGraph, END
-from .supervisor_state import SupervisorState
-from .supervisor_agent import SupervisorAgent
+from agents.supervisor.supervisor_state import SupervisorState
+from agents.supervisor.supervisor_agent import SupervisorAgent
+from langgraph.checkpoint.sqlite import SqliteSaver
 
 class SupervisorWorkflow():
-    def __init__(self, llm):
-        self.agent = SupervisorAgent(llm, "Do Something")
-        supervisor_workflow = StateGraph(SupervisorState)
+    def __init__(self, llm, collections, thread_id, members, memberids, user_input, rag_try_limit, project_path):
+        self.collections = collections
+        self.thread_id = thread_id
+        self.members = members
+        self.memberids = memberids
+        self.user_input = user_input
+        self.memory = None
+        if thread_id is not None:
+            self.memory = SqliteSaver.from_conn_string(":memory:")
+        self.agent = SupervisorAgent(llm, self.collections, self.members, self.memberids, self.user_input, rag_try_limit, project_path)
+        self.supervisor_workflow = StateGraph(SupervisorState)
         # Define the nodes
-        supervisor_workflow.add_node("kickoff", self.agent.instantiate_team_members)
-        supervisor_workflow.add_node("Architect", self.agent.call_architect)
-        supervisor_workflow.add_node("Coder", self.agent.call_coder)
-        supervisor_workflow.add_node("RAG", self.agent.call_rag)
-        supervisor_workflow.add_node("Planner", self.agent.call_planner)
-        supervisor_workflow.add_node("update_state", self.agent.update_state)
-        
+        self.supervisor_workflow.add_node("kickoff", self.agent.instantiate_team_members)
+        self.supervisor_workflow.add_node("Architect", self.agent.call_architect)
+        self.supervisor_workflow.add_node("Coder", self.agent.call_coder)
+        self.supervisor_workflow.add_node("RAG", self.agent.call_rag)
+        self.supervisor_workflow.add_node("Planner", self.agent.call_planner)
+        self.supervisor_workflow.add_node("update_state", self.agent.update_state)
+        self.supervisor_workflow.add_node("Supervisor", self.agent.call_supervisor)
+        self.supervisor_workflow.add_node("Human", self.agent.call_human)
+
         # Build graph
-        supervisor_workflow.set_entry_point("kickoff")
-        supervisor_workflow.add_conditional_edges("kickoff",
+        self.supervisor_workflow.set_entry_point("kickoff")
+        self.supervisor_workflow.add_edge("kickoff","Supervisor")
+        self.supervisor_workflow.add_conditional_edges("Supervisor",
                                                   self.agent.delegator,
                                                   {
-                                                      "Architect" : "Architect",
-                                                      "Coder" : "Coder",
-                                                      "RAG" : "RAG",
-                                                      "Planner" : "Planner",
-                                                      "update_state" : "update_state"
+                                                      "call_architect" : "Architect",
+                                                      "call_coder" : "Coder",
+                                                      "call_rag" : "RAG",
+                                                      "call_planner" : "Planner",
+                                                      "call_supervisor": 'Supervisor',
+                                                      "update_state" : "update_state",
+                                                      "Human" : 'Human'
                                                   })
-        supervisor_workflow.add_edge("update_state",END)
-
-        # Compile
-        supervisor_workflow.sup_app = supervisor_workflow.compile()
+        self.supervisor_workflow.add_edge("RAG","Supervisor")
+        self.supervisor_workflow.add_edge("Architect","Supervisor")
+        self.supervisor_workflow.add_edge("Planner","Supervisor")
+        self.supervisor_workflow.add_edge("Coder","Supervisor")
+        self.supervisor_workflow.add_edge("Human","Supervisor")
+        self.supervisor_workflow.add_edge("update_state",END)
+        # # Compile
+        # supervisor_workflow.sup_app = supervisor_workflow.compile()
+        if self.thread_id is not None:
+            self.sup_app = self.supervisor_workflow.compile(checkpointer=self.memory)
+        
+        else:
+            print("You have not set the Thread ID so not persisting the workflow state.")
+            self.sup_app = self.supervisor_workflow.compile()
 
     def get_current_state(self):
         """ Returns the current state dictionary of the agent """
