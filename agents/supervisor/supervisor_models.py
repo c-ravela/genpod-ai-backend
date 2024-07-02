f1fa570ce2112445a06edcd1d720f2fb94a977b1
VishwasSomasekhariah <vkumar@intelops.dev>
2024-07-02 11:32:53 -0500

    Adding a QueryList model and Policy to be used by supervisor agent

---
+++ agents/supervisor/supervisor_models.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, Field, field_validator
+from typing import List
+
+class QueryList(BaseModel):
+    req_queries: list[str]
+
+    @field_validator('req_queries')
+    @classmethod
+    def check_req_queries_items(cls, v):
+        if not isinstance(v, list):
+            raise ValueError('QueryList must be a list')
+        for item in v:
+            if not isinstance(item, str):
+                raise ValueError('Each req_queries item must be a string')
+        return v
