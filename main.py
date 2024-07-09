760802382893360dbac45b9064b37050c4c0afe8
VishwasSomasekhariah <vkumar@intelops.dev>
2024-07-09 14:22:16 -0500

    Update main driver program to use additional user input needed for coder

--- main.py
+++ main.py
@@ -16,11 +16,16 @@ if __name__=="__main__":
     PROJECT_INPUT = "I want to develop a Title Requests Micro-service adhering to MISMO v3.6 standards to handle get_title service using GET REST API Call in .NET?"
     # PROJECT_INPUT = "How to make money?"
     PROJECT_PATH = "/"
+    LICENSE_URL = "https://raw.githubusercontent.com/intelops/tarian-detector/8a4ff75fe31c4ffcef2db077e67a36a067f1437b/LICENSE"
+    LICENSE_TEXT = "This code base copyrights belong to XYZ"
     LLM = ChatOpenAI(model="gpt-4o-2024-05-13", temperature=0.2, max_retries=5, streaming=True, seed=4000, top_p=0.8)
     SUPERVISOR_THREAD_ID = "999"
     PROJECT_PATH = '/'
     SUPERVISOR = SupervisorWorkflow(LLM, VECTOR_DB_COLLECTIONS, SUPERVISOR_THREAD_ID, MEMBERS, THREAD_IDS, PROJECT_INPUT, RAG_TRY_LIMIT, PROJECT_PATH)
 
     config = {"configurable":{"thread_id":SUPERVISOR_THREAD_ID}}
-    result = SUPERVISOR.sup_app.invoke({"messages": [("Human", PROJECT_INPUT)]}, config)
+    result = SUPERVISOR.sup_app.invoke({"messages": [("Human", PROJECT_INPUT)],
+                                        'license_url': LICENSE_URL,
+                                        'license_text':LICENSE_TEXT},
+                                        config)
     pprint(result)
