155c70653ba2fff875e4d9926fa2d88320fd3692
charan ravela <cravela@intelops.dev>
2024-07-26 13:45:08 -0500

    feat: add new function

--- configs/persistence_db.py
+++ configs/persistence_db.py
@@ -1,16 +1,26 @@
 """
 This module manages the persistence of data for a single agent using a 
 SQLite database.
-
-Database: genpod.db
 """
 
 import os
 
-db_dir = os.path.join(os.getcwd(), "output", "database")
+db_dir = os.path.join(os.getcwd(), "output", "databases")
 
-if not os.path.exists(db_dir):
-    os.makedirs(db_dir)
+def set_persistance_db(name:str="genpod", timestamp: str="") -> str:
+    """
+    This function set the path for db.
 
-# Define the path to the database file
-PERSISTANCE_DB_PATH = os.path.join(db_dir, "genpod.db")
+    timestamp: current_time
+    name: name of the database without ".db"
+
+    return: db path
+    """
+
+    db_dir_with_timestamp = os.path.join(db_dir, timestamp)
+    if not os.path.exists(db_dir_with_timestamp):
+        os.makedirs(db_dir_with_timestamp)
+
+    return os.path.join(db_dir_with_timestamp, f"{name}.db")
+
+PERSISTANCE_DB_PATH = os.path.join(db_dir, "genpod_test.db")
