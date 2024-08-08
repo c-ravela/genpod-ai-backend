a3a95a77bbf8b6fd4953360d8663b3b7f1bd5978
charan ravela <cravela@intelops.dev>
2024-08-08 15:59:01 -0500

    feat: updated code

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 """
 Driving code file for this project.
 """
-
 import os
 from pprint import pprint
 
 from agents.supervisor.supervisor_graph import SupervisorWorkflow
-from configs.database import get_client_local_db_file_path
+
 from configs.project_config import ProjectConfig
 from configs.project_path import set_project_path
+from configs.database import get_client_local_db_file_path
 from configs.supervisor_config import RAG_TRY_LIMIT, THREAD_IDS
+
 from database.database import Database
-from utils.logs.logging_utils import logger
+
 from utils.time import get_timestamp
+from utils.logs.logging_utils import logger
 
 print("\n\nWe greatly appreciate your interest! Please note that we are in the midst of active development and are striving to make improvements every day!\n\n")
 
 if __name__=="__main__":
     TIME_STAMP = get_timestamp()
+    logger.info(f"Project Generation has been triggered at {TIME_STAMP}!")
+
+    # Initialize the project config
     config = ProjectConfig()
+    logger.info("Project configuration loaded!")
 
     USER_ID = int(os.getenv("USER_ID"))
     if USER_ID is None:
@@ -66,25 +72,24 @@ if __name__=="__main__":
     
     supervisor_info = config.agents.supervisor
     SUPERVISOR = SupervisorWorkflow(
-        config.agents_config[supervisor_info.agent_id].llm, 
+        config.agents_config[supervisor_info.agent_id].llm,
         config.vector_db_collections, 
-        config.agents_config[supervisor_info.agent_id].thread_id, 
-        config.agents_config, 
-        THREAD_IDS, 
         PROJECT_INPUT, 
         RAG_TRY_LIMIT, 
         PROJECT_PATH, 
-        DATABASE_PATH
+        DATABASE_PATH,
+        config.agents_config
     )
 
-    config = {"configurable":{"thread_id": config.agents_config[supervisor_info.agent_id].thread_id}, "recursion_limit":500}
-    # result = SUPERVISOR.sup_app.invoke({
-    #     "messages": [("Human", PROJECT_INPUT)],
-    #     'license_url': LICENSE_URL,
-    #     'license_text':LICENSE_TEXT
-    #     }, 
-    #     config
-    # )
+    sup_config = {"configurable":{"thread_id": config.agents_config[supervisor_info.agent_id].thread_id}, "recursion_limit":500}
+    result = SUPERVISOR.workflow.invoke({
+        "messages": [("Human", PROJECT_INPUT)],
+        'license_url': LICENSE_URL,
+        'license_text':LICENSE_TEXT
+        }, 
+        sup_config
+    )
     
-    pprint(config)
-  
+    pprint(result)
+
+    # TODO: once state is returned update db with returned value
