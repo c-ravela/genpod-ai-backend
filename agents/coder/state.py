8487af6860b5190b011cd3e12266056176f8773a
charan ravela <cravela@intelops.dev>
2024-08-15 18:20:13 -0500

    feat: updated code

--- agents/coder/state.py
+++ agents/coder/state.py
@@ -6,6 +6,7 @@ from typing_extensions import Annotated, TypedDict
 
 from models.models import Task
 
+from agents.agent.state import State
 
 class CoderState(TypedDict):
     """
@@ -14,78 +15,81 @@ class CoderState(TypedDict):
     # @in 
     project_name: Annotated[
         str, 
-        "The name of the project."
+        State.in_field("The name of the project.")
     ]
 
     # @in 
     project_folder_strucutre: Annotated[
         str,
-        "The organized layout of directories and subdirectories that form the project's "
-        "file system, adhering to best practices for project structure."
+        State.in_field("The organized layout of directories and subdirectories that form the project's "
+        "file system, adhering to best practices for project structure.")
     ]
 
     # @in 
-    requirements_overview: Annotated[
+    requirements_document: Annotated[
         str, 
-        "A comprehensive, well-structured document in markdown format that outlines "
+        State.in_field("A comprehensive, well-structured document in markdown format that outlines "
         "the project's requirements derived from the user's request. This serves as a "
-        "guide for the development process."
+        "guide for the development process.")
     ]
 
     # @in
     generated_project_path: Annotated[
         str,
-        "The absolute path in the file system where the project is being generated. "
-        "This path is used to store all the project-related files and directories."
+        State.in_field("The absolute path in the file system where the project is being generated. "
+        "This path is used to store all the project-related files and directories.")
     ]
 
     # @in
     license_url: Annotated[
         str,
-        ""
+        State.in_field()
     ]
 
     # @in
-    license_text: Annotated[str, ""]
+    license_text: Annotated[
+        str, 
+        State.in_field()
+    ]
 
     # @inout
     current_task: Annotated[
         Task,
-        "The Task object currently in focus, representing the active task that team "
-        "members are working on."
+        State.inout_field("The Task object currently in focus, representing the active task that team "
+        "members are working on.")
     ]
     
     # @inout
     messages: Annotated[
         list[tuple[str, str]], 
-        "A chronological list of tuples representing the conversation history between the "
+        State.inout_field("A chronological list of tuples representing the conversation history between the "
         "system, user, and AI. Each tuple contains a role identifier (e.g., 'AI', 'tool', "
-        "'user', 'system') and the corresponding message."
+        "'user', 'system') and the corresponding message.")
     ]
 
     # @out
     code: Annotated[
         str, 
-        "The complete, well-documented working code that adheres to all standards "
-        "requested with the programming language, framework user requested ",
+        State.out_field("The complete, well-documented working code that adheres to all standards "
+        "requested with the programming language, framework user requested ")
     ]
 
     # @out
     files_created: Annotated[
         list[str], 
-        "The absolute paths of file that were created for this project "
-        "so far."
+        State.out_field("The absolute paths of file that were created for this project "
+        "so far.")
     ]
 
     # @out
     infile_license_comments: Annotated[
         dict[str, str],
-        "A list of multiline license comments for each type of file."
+        State.out_field("A list of multiline license comments for each type of file.")
     ]
 
+    # @out
     commands_to_execute: Annotated[ 
         dict[str, str],
-        """
-        This field represents a dictionary of commands intended to be executed on a Linux terminal. Each key-value pair in the dictionary corresponds to an absolute path (the key) and a specific command (the value) to be executed at that path.
-        """
+        State.out_field("This field represents a dictionary of commands intended to be "
+        "executed on a Linux terminal. Each key-value pair in the dictionary corresponds to an absolute path (the key) and a specific command (the value) to be executed at that path.")
     ]
