3a0a762d342983ae161b396fc0cdbd69e4e8dbd6
charan ravela <cravela@intelops.dev>
2024-09-06 13:51:54 -0500

    feat: update graph

--- agents/planner/planner_graph.py
+++ agents/planner/planner_graph.py
@@ -22,39 +22,31 @@ class PlannerWorkFlow(Graph[PlannerAgent]):
     def define_graph(self) -> StateGraph:
         
         planner_workflow = StateGraph(PlannerState, config_schema=Task)
+        
         # Define the nodes
-        # self.planner_workflow.add_node("new_deliverable_check", self.agent.new_deliverable_check)
         planner_workflow.add_node("backlog_planner", self.agent.backlog_planner)  # retrieve
         planner_workflow.add_node("requirements_developer", self.agent.requirements_developer)  # grade documents
-        planner_workflow.add_node("response_generator",self.agent.generate_response)
+        planner_workflow.add_node("response_generator", self.agent.generate_response)
         planner_workflow.add_node("update_state", self.agent.update_state)
 
-        # Build graph
-        planner_workflow.set_conditional_entry_point(self.agent.new_deliverable_check,
-                                                          {"backlog_planner": "backlog_planner",
-                                                           "requirements_developer": "requirements_developer"})
-
-        # self.planner_workflow.set_conditional_entry_point(
-        #     self.agent.new_deliverable_check,
-        #     {
-        #         "yes": "backlog_planner",
-        #         "no": "requirements_developer"
-        #     }
-        # )
+        # Define edges
         planner_workflow.add_edge("backlog_planner", "requirements_developer")
-        planner_workflow.add_edge("requirements_developer", "update_state")
-        planner_workflow.add_edge("update_state","response_generator")
-        planner_workflow.add_edge("response_generator", END)
+        planner_workflow.add_edge("requirements_developer", "response_generator")
+        planner_workflow.add_edge("response_generator", "update_state")
+        planner_workflow.add_edge("update_state", END)
+
+        # Define entry point
+        planner_workflow.set_conditional_entry_point(
+            self.agent.new_deliverable_check,
+            {
+                "backlog_planner": "backlog_planner",
+                "requirements_developer": "requirements_developer"
+            }
+        )
 
         return planner_workflow
-    
+
     def get_current_state(self):
         """ Returns the current state dictionary of the agent """
-        return self.agent.state
 
-    def update_task(self, incoming_task: Task):
-        try:
-            self.agent.current_task = incoming_task
-            return "Successfully added the task to memory"
-        except: 
-            return "Unable to add the task to memory"
+        return self.agent.state
