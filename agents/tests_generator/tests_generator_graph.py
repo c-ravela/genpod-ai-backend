6b052bab349dd8ed7b417df0673237fc183a7bb1
charan ravela <cravela@intelops.dev>
2024-10-29 13:27:46 -0500

    feat: fix linting issues

--- agents/tests_generator/tests_generator_graph.py
+++ agents/tests_generator/tests_generator_graph.py
@@ -1,6 +1,4 @@
-"""
-TestCoder Graph
-"""
+"""TestCoder Graph"""
 
 from langgraph.graph import END, StateGraph
 
@@ -14,7 +12,15 @@ class TestCoderGraph(Graph[TestCoderAgent]):
     """
     """
 
-    def __init__(self, graph_id: str, graph_name: str, agent_id: str, agent_name: str, llm: LLM, persistance_db_path: str) -> None:
+    def __init__(
+            self,
+            graph_id: str,
+            graph_name: str,
+            agent_id: str,
+            agent_name: str,
+            llm: LLM,
+            persistance_db_path: str
+        ) -> None:
         """
         """
 
@@ -32,41 +38,87 @@ class TestCoderGraph(Graph[TestCoderAgent]):
         unit_test_coder_flow = StateGraph(TestCoderState)
 
         # node
-        unit_test_coder_flow.add_node(self.agent.entry_node_name, self.agent.entry_node)
-        unit_test_coder_flow.add_node(self.agent.skeleton_generation_node_name,self.agent.skeleton_generation_node)
-        unit_test_coder_flow.add_node(self.agent.test_code_generation_node_name, self.agent.test_code_generation_node)
-        unit_test_coder_flow.add_node(self.agent.skeleton_updation_node_name,self.agent.skeleton_updation_node)
-        unit_test_coder_flow.add_node(self.agent.test_code_updation_node_name, self.agent.test_code_updation_node)
-        unit_test_coder_flow.add_node(self.agent.write_skeleton_node_name, self.agent.write_skeleton_node)
-        unit_test_coder_flow.add_node(self.agent.write_generated_code_node_name, self.agent.write_code_node)
-        unit_test_coder_flow.add_node(self.agent.update_state_node_name, self.agent.update_state)
+        unit_test_coder_flow.add_node(
+            self.agent.entry_node_name,
+            self.agent.entry_node
+        )
+        unit_test_coder_flow.add_node(
+            self.agent.skeleton_generation_node_name,
+            self.agent.skeleton_generation_node
+        )
+        unit_test_coder_flow.add_node(
+            self.agent.test_code_generation_node_name,
+            self.agent.test_code_generation_node
+        )
+        unit_test_coder_flow.add_node(
+            self.agent.skeleton_updation_node_name,
+            self.agent.skeleton_updation_node
+        )
+        unit_test_coder_flow.add_node(
+            self.agent.test_code_updation_node_name,
+            self.agent.test_code_updation_node
+        )
+        unit_test_coder_flow.add_node(
+            self.agent.write_skeleton_node_name,
+            self.agent.write_skeleton_node
+        )
+        unit_test_coder_flow.add_node(
+            self.agent.write_generated_code_node_name,
+            self.agent.write_code_node
+        )
+        unit_test_coder_flow.add_node(
+            self.agent.update_state_node_name, 
+            self.agent.update_state
+        )
 
         # edges
         unit_test_coder_flow.add_conditional_edges(
             self.agent.entry_node_name,
             self.agent.router,
             {
-                self.agent.skeleton_generation_node_name: self.agent.skeleton_generation_node_name,
-                self.agent.skeleton_updation_node_name: self.agent.skeleton_updation_node_name,
+                self.agent.skeleton_generation_node_name:
+                    self.agent.skeleton_generation_node_name,
+                self.agent.skeleton_updation_node_name:
+                    self.agent.skeleton_updation_node_name,
             }
         )
 
-        unit_test_coder_flow.add_edge(self.agent.skeleton_generation_node_name, self.agent.write_skeleton_node_name)
-        unit_test_coder_flow.add_edge(self.agent.skeleton_updation_node_name, self.agent.write_skeleton_node_name)
+        unit_test_coder_flow.add_edge(
+            self.agent.skeleton_generation_node_name,
+            self.agent.write_skeleton_node_name
+        )
+        unit_test_coder_flow.add_edge(
+            self.agent.skeleton_updation_node_name,
+            self.agent.write_skeleton_node_name
+        )
 
         unit_test_coder_flow.add_conditional_edges(
             self.agent.write_skeleton_node_name,
             self.agent.router,
             {
-                self.agent.test_code_generation_node_name: self.agent.test_code_generation_node_name,
-                self.agent.test_code_updation_node_name: self.agent.test_code_updation_node_name
+                self.agent.test_code_generation_node_name: 
+                    self.agent.test_code_generation_node_name,
+                self.agent.test_code_updation_node_name: 
+                    self.agent.test_code_updation_node_name
             }
         )
 
-        unit_test_coder_flow.add_edge(self.agent.test_code_generation_node_name, self.agent.write_generated_code_node_name)
-        unit_test_coder_flow.add_edge(self.agent.test_code_updation_node_name, self.agent.write_generated_code_node_name)
-        unit_test_coder_flow.add_edge(self.agent.write_generated_code_node_name, self.agent.update_state_node_name)
-        unit_test_coder_flow.add_edge(self.agent.update_state_node_name, END)
+        unit_test_coder_flow.add_edge(
+            self.agent.test_code_generation_node_name,
+            self.agent.write_generated_code_node_name
+        )
+        unit_test_coder_flow.add_edge(
+            self.agent.test_code_updation_node_name,
+            self.agent.write_generated_code_node_name
+        )
+        unit_test_coder_flow.add_edge(
+            self.agent.write_generated_code_node_name,
+            self.agent.update_state_node_name
+        )
+        unit_test_coder_flow.add_edge(
+            self.agent.update_state_node_name,
+            END
+        )
 
         # entry point
         unit_test_coder_flow.set_entry_point(self.agent.entry_node_name)
@@ -80,6 +132,5 @@ class TestCoderGraph(Graph[TestCoderAgent]):
         Returns:
             ArchitectState: The current state of the Architect agent.
         """
-        
+     
         return self.agent.state
-    
