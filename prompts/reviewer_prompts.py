86160c01861a5b127dc8a2f3bafbbcaf65363cb9
charan ravela <cravela@intelops.dev>
2024-10-29 13:11:47 -0500

    feat: fix lint issues

--- prompts/reviewer_prompts.py
+++ prompts/reviewer_prompts.py
@@ -1,6 +1,5 @@
-"""
-Reviewer Prompts
-"""
+"""Reviewer Prompts"""
+
 from langchain.output_parsers import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 
@@ -8,18 +7,18 @@ from models.reviewer_models import ReviewerOutput
 
 
 class ReviewerPrompts:
-    """
-    This class contains prompts used by reviewers.
-    """
+    """This class contains prompts used by reviewers."""
 
     static_code_analysis_prompt: PromptTemplate = PromptTemplate(
         template="""
-        As a skilled reviewer, you play a crucial role in delivering a comprehensive end-to-end project as per the user's request. 
-        Your expertise is in reviewing and analyzing the work done by other team members.
+        As a skilled reviewer, you play a crucial role in delivering a comprehensive 
+        end-to-end project as per the user's request. Your expertise is in reviewing 
+        and analyzing the work done by other team members.
 
         You have been provided with the output from the `{static_analysis_tool}`.
 
-        Error message will be provided if we encounter an issue with your previous response:
+        Error message will be provided if we encounter an issue with your previous 
+        response:
         `{error_message}`
 
         Tool results:
@@ -32,13 +31,15 @@ class ReviewerPrompts:
         {format_instructions}
         ```
 
-        Your task is to generate any issues based on the results provided. Ensure that the issues are clearly detailed and 
-        formatted according to the given instructions. Focus solely on project-related issues, excluding any concerns about 
-        the tool itself.
+        Your task is to generate any issues based on the results provided. Ensure that 
+        the issues are clearly detailed and formatted according to the given instructions. 
+        Focus solely on project-related issues, excluding any concerns about the tool itself.
         """,
         input_variables= ['static_analysis_tool', 'tool_result', 'error_message'],
         partial_variables={
-            "format_instructions": PydanticOutputParser(pydantic_object=ReviewerOutput).get_format_instructions()
+            "format_instructions": PydanticOutputParser(
+                pydantic_object=ReviewerOutput
+            ).get_format_instructions()
         }
     )
-    
+  
