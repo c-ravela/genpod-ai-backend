d97a97fb95bc76c18b7736617bf90762b670cb23
charan ravela <cravela@intelops.dev>
2024-10-29 13:20:22 -0500

    feat: fix lint issues

--- models/models.py
+++ models/models.py
@@ -1,9 +1,9 @@
 """
 This module, `models.py`, contains various data models used throughout the project.
 
-Each class in this module represents a different data model, with each model 
+Each class in this module represents a different data model, with each model
 capturing a specific set of information required for the project. These models
-are used to structure the data in a consistent and organized manner, enhancing 
+are used to structure the data in a consistent and organized manner, enhancing
 the readability and maintainability of the code.
 """
 from typing import Any, Generic, Iterator, List, Optional, TypeVar
@@ -11,16 +11,17 @@ from typing import Any, Generic, Iterator, List, Optional, TypeVar
 from pydantic import BaseModel, Field
 
 from models.constants import Status
-from models.tests_generator_models import (FileFunctionSignatures,
-                                           TestCodeGeneration)
+from models.tests_generator_models import FileFunctionSignatures
 from utils.task_utils import generate_task_id
 
 QueueType = TypeVar("QueueType", bound=BaseModel)
 TQueue = TypeVar('TQueue', bound='Queue')
 
+
 class Queue(BaseModel, Generic[QueueType]):
     """
-    A base class to manage a queue of items with an index to keep track of the next item to process.
+    A base class to manage a queue of items with an index to keep track of the next 
+    item to process.
     """
 
     next: int = Field(
@@ -36,7 +37,7 @@ class Queue(BaseModel, Generic[QueueType]):
     def add_item(self, item: QueueType) -> None:
         """
         Adds a new item to the end of the queue.
-        
+       
         Args:
             item (QueueType): The item to be added.
         """
@@ -45,7 +46,7 @@ class Queue(BaseModel, Generic[QueueType]):
     def extend(self: TQueue, queue: TQueue) -> None:
         """
         Extends the current queue with the items from another queue of the same type.
-        
+       
         Args:
             other (TQueue): Another instance of the same Queue subclass.
         """
@@ -54,21 +55,22 @@ class Queue(BaseModel, Generic[QueueType]):
     def get_next_item(self) -> Optional[QueueType]:
         """
         Retrieves and advances to the next item in the queue.
-        
+       
         Returns:
-            Optional[QueueType]: The next item in the queue, or None if no items are left.
+            Optional[QueueType]: The next item in the queue, or None if no items are
+            left.
         """
         if self.next < len(self.items):
             item = self.items[self.next]
             self.next += 1
             return item
-        
+       
         return None
 
     def get_all_items(self) -> List[QueueType]:
         """
         Returns a list of all items in the queue.
-        
+       
         Returns:
             List[QueueType]: A list containing all items in the queue.
         """
@@ -77,30 +79,30 @@ class Queue(BaseModel, Generic[QueueType]):
     def update_item(self, updated_item: QueueType) -> None:
         """
         Updates an existing item in the queue with the new values from the updated_item.
-        
+       
         This method must be implemented by subclasses.
-        
+      
         Args:
             updated_item (QueueType): The updated item with new values.
-        
+      
         Raises:
             NotImplementedError: If this method is not overridden in a subclass.
         """
         raise NotImplementedError("Subclasses must implement this method.")
-    
+  
     def has_pending_items(self) -> bool:
         """
         Checks if there are any pending items that have not yet been processed.
-        
+     
         Returns:
             bool: True if there are unprocessed items, False otherwise.
         """
         return self.next < len(self.items)
-    
+  
     def __str__(self) -> str:
         """
         Returns a string representation of the Queue.
-        
+       
         Returns:
             str: A string representing the current items and the index of the next item.
         """
@@ -109,7 +111,7 @@ class Queue(BaseModel, Generic[QueueType]):
     def __iter__(self) -> Iterator[QueueType]:
         """
         Returns an iterator over the items in the queue.
-        
+       
         Returns:
             Iterator[QueueType]: An iterator for the items in the queue.
         """
@@ -118,25 +120,30 @@ class Queue(BaseModel, Generic[QueueType]):
     def __len__(self) -> int:
         """
         Returns the number of items in the queue.
-        
+      
         Returns:
             int: The number of items in the queue.
         """
         return len(self.items)
-    
+
+  
 class Task(BaseModel):
     """
     A data model representing a task and its current state within a project
     or workflow.
     """
+
     task_id: str = Field(
-        description="A unique task id to track and access current task and previous tasks",
+        description=(
+            "A unique task id to track and access current task and previous "
+            "tasks"
+        ),
         default_factory=generate_task_id
     )
 
     task_status: Status = Field(
         description="The current status indicating the progress of the task",
-        default= Status.NONE,
+        default=Status.NONE,
         required=True
     )
 
@@ -157,18 +164,17 @@ class Task(BaseModel):
         default=""
     )
 
+
 class TaskQueue(Queue[Task]):
-    """
-    A queue specifically for Task objects.
-    """
-    
+    """A queue specifically for Task objects."""
+   
     def update_item(self, updated_task: Task) -> None:
         """
         Updates an existing task in the queue with the new values from the updated_task.
 
         Args:
             updated_task (Task): The updated Task object with new values.
-        
+       
         Raises:
             ValueError: If the task to be updated is not found in the queue.
         """
@@ -177,6 +183,7 @@ class TaskQueue(Queue[Task]):
                 self.items[i] = updated_task
                 return
         raise ValueError(f"Task with ID {updated_task.task_id} not found in the queue.")
+
 
 class PlannedTask(BaseModel):
     """
@@ -190,13 +197,16 @@ class PlannedTask(BaseModel):
     )
 
     task_id: str = Field(
-        description="A unique task id to track and access current task and previous tasks",
+        description=(
+            "A unique task id to track and access current task and previous"
+            "tasks"
+        ),
         default_factory=generate_task_id
     )
 
     task_status: Status = Field(
         description="The current status indicating the progress of the task",
-        default= Status.NONE,
+        default=Status.NONE,
         required=True
     )
 
@@ -224,18 +234,18 @@ class PlannedTask(BaseModel):
         required=True
     )
 
+
 class PlannedTaskQueue(Queue[PlannedTask]):
-    """
-    A queue specifically for PlannedTask objects.
-    """
+    """A queue specifically for PlannedTask objects."""
 
     def update_item(self, updated_task: PlannedTask) -> None:
         """
-        Updates an existing planned task in the queue with the new values from the updated_task.
+        Updates an existing planned task in the queue with the new values from the
+        updated_task.
 
         Args:
             updated_task (PlannedTask): The updated PlannedTask object with new values.
-        
+
         Raises:
             ValueError: If the planned task to be updated is not found in the queue.
         """
@@ -243,12 +253,15 @@ class PlannedTaskQueue(Queue[PlannedTask]):
             if task.task_id == updated_task.task_id:
                 self.items[i] = updated_task
                 return
-            
-        raise ValueError(f"PlannedTask with ID {updated_task.task_id} not found in the queue.")
+
+        raise ValueError(
+            f"PlannedTask with ID {updated_task.task_id} not found in the queue."
+        )
+
 
 class Issue(BaseModel):
     """
-    A model representing an issue with various attributes including status, description, 
+    A model representing an issue with various attributes including status, description,
     and file path.
     """
 
@@ -288,43 +301,52 @@ class Issue(BaseModel):
         default=None,
         description="Suggestions for resolving the issue.",
         title="Suggestions",
-        examples=[ "Define the variable 'x' before use", "Check variable scope and initialization" ]
+        examples=[ 
+            "Define the variable 'x' before use", 
+            "Check variable scope and initialization"
+        ]
     )
 
     def issue_details(self) -> str:
         """Return a formatted string representing the actual issue details."""
         details = f"Issue: {self.description}\n"
         details += f"File: {self.file_path}\n"
-        
+      
         if self.line_number:
             details += f"Line: {self.line_number}\n"
-        
+     
         if self.suggestions:
             suggestions_str = "\n".join(self.suggestions)
             details += f"Suggestions:\n{suggestions_str}\n"
-        
+   
         return details
+
 
 class IssuesQueue(Queue[Issue]):
     """
-    A specialized queue for managing issues, allowing updates to existing issues within the queue.
+    A specialized queue for managing issues, allowing updates to existing 
+    issues within the queue.
     """
-    
+
     def update_item(self, updated_issue: Issue) -> None:
         """
         Update an existing issue in the queue with the provided updated issue.
 
         Args:
-            updated_issue (Issue): The updated issue to replace the existing issue in the queue.
+            updated_issue (Issue): The updated issue to replace the existing 
+            issue in the queue.
 
         Raises:
-            ValueError: If the issue with the specified ID is not found in the queue.
+            ValueError: If the issue with the specified ID is not found in the 
+            queue.
         """
         for i, issue in enumerate(self.items):
             if issue.issue_id == updated_issue.issue_id:
                 self.items[i] = updated_issue
                 return
-        raise ValueError(f"Issue with ID {updated_issue.issue_id} not found in the queue.")
+        raise ValueError(f"Issue with ID {updated_issue.issue_id} not found in the "
+                         "queue.")
+
 
 class PlannedIssue(BaseModel):
     parent_id: str = Field(
@@ -348,7 +370,8 @@ class PlannedIssue(BaseModel):
     )
 
     is_test_code_generated: bool = Field(
-        description="Indicates whether unit test cases have been generated for this task.",
+        description="Indicates whether unit test cases have been generated for this "
+                    "task.",
         default=False
     )
 
@@ -382,11 +405,15 @@ class PlannedIssue(BaseModel):
         default=[],
         description="Suggestions for resolving the issue.",
         title="Suggestions",
-        examples=[ "Define the variable 'x' before use", "Check variable scope and initialization" ]
+        examples=[
+            "Define the variable 'x' before use", 
+            "Check variable scope and initialization"
+        ]
     )
 
     function_signatures: FileFunctionSignatures = Field(
-        description="The skeleton or template of the function that needs to be implemented.",
+        description="The skeleton or template of the function that needs to be "
+                    "implemented.",
         default="",
         examples=["def function_name(args): pass"]
     )
@@ -401,27 +428,30 @@ class PlannedIssue(BaseModel):
         """Return a formatted string representing the actual issue details."""
         details = f"Issue: {self.description}\n"
         details += f"File: {self.file_path}\n"
-        
+
         if self.line_number:
             details += f"Line: {self.line_number}\n"
-        
+
         if self.suggestions:
             suggestions_str = "\n".join(self.suggestions)
             details += f"Suggestions:\n{suggestions_str}\n"
-        
+
         return details
+
 
 class PlannedIssuesQueue(Queue[PlannedIssue]):
     """
-    A specialized queue for managing issues, allowing updates to existing issues within the queue.
+    A specialized queue for managing issues, allowing updates to existing issues 
+    within the queue.
     """
-    
+
     def update_item(self, updated_issue: PlannedIssue) -> None:
         """
         Update an existing issue in the queue with the provided updated issue.
 
         Args:
-            updated_issue (PlannedIssue): The updated issue to replace the existing issue in the queue.
+            updated_issue (PlannedIssue): The updated issue to replace the existing
+            issue in the queue.
 
         Raises:
             ValueError: If the issue with the specified ID is not found in the queue.
@@ -432,60 +462,75 @@ class PlannedIssuesQueue(Queue[PlannedIssue]):
                 return
         raise ValueError(f"Issue with ID {updated_issue.id} not found in the queue.")
 
+
 class RequirementsDocument(BaseModel):
     """
-    Represents a comprehensive document that encapsulates the various requirements of a project. 
-    This class includes details about the project's architecture, tasks, coding standards, 
-    implementation process, and licensing information. It also provides methods to generate
-    a Markdown representation of the document.
+    Represents a comprehensive document that encapsulates the various
+    requirements of a project. This class includes details about the
+    project's architecture, tasks, coding standards, implementation
+    process, and licensing information. It also provides methods to
+    generate a Markdown representation of the document.
     """
 
     project_overview: str = Field(
-        description="A brief overview of the project, summarizing its goals and objectives.",
+        description="A brief overview of the project, summarizing its goals "
+                    "and objectives.",
         default=""
     )
-    
+
     project_architecture: str = Field(
-        description="Detailed information about the project's architecture, including design patterns and key components.",
+        description="Detailed information about the project's architecture, "
+                    "including design patterns and key components.",
         default=""
     )
 
     directory_structure: str = Field(
-        description="A description of the project's directory and folder structure, outlining the organization of files and directories.",
+        description="A description of the project's directory and folder "
+                    "structure, outlining the organization of files and "
+                    "directories.",
         default=""
     )
 
     microservices_architecture: str = Field(
-        description="Details about the design and architecture of the project's microservices, including their interactions and dependencies.",
+        description="Details about the design and architecture of the "
+                    "project's microservices, including their interactions "
+                    "and dependencies.",
         default=""
     )
 
     tasks_overview: str = Field(
-        description="An overview of the tasks involved in the project, including their purpose and key objectives.",
+        description="An overview of the tasks involved in the project, "
+                    "including their purpose and key objectives.",
         default=""
     )
 
     coding_standards: str = Field(
-        description="The coding standards and conventions followed in the project, ensuring consistency and quality in the codebase.",
+        description="The coding standards and conventions followed in the "
+                    "project, ensuring consistency and quality in the "
+                    "codebase.",
         default=""
     )
 
     implementation_process: str = Field(
-        description="A detailed description of the implementation process, including phases, milestones, and methodologies.",
+        description="A detailed description of the implementation process, "
+                    "including phases, milestones, and methodologies.",
         default=""
     )
 
     project_license_information: str = Field(
-        description="Information about the project's licensing terms and conditions, including usage rights and restrictions.",
+        description="Information about the project's licensing terms and "
+                    "conditions, including usage rights and restrictions.",
         default=""
     )
 
     def to_markdown(self) -> str:
         """
-        Generates a Markdown-formatted string representing the requirements document.
+        Generates a Markdown-formatted string representing the requirements
+        document.
 
         Returns:
-            str: A Markdown string that represents the requirements document, formatted with sections for each attribute.
+            str: A Markdown string that represents the requirements document,
+            formatted with sections for each attribute.
         """
 
         return f"""
@@ -515,7 +560,7 @@ class RequirementsDocument(BaseModel):
 ## Project License Information
 {self.project_license_information}
         """
-    
+
     def __getitem__(self, key: str) -> Any:
         """
         Retrieves the value of an attribute using square bracket notation.
@@ -527,7 +572,8 @@ class RequirementsDocument(BaseModel):
             Any: The value of the specified attribute.
 
         Raises:
-            KeyError: If the attribute with the specified key does not exist in the document.
+            KeyError: If the attribute with the specified key does not exist 
+            in the document.
         """
         if hasattr(self, key):
             return getattr(self, key)
@@ -542,7 +588,8 @@ class RequirementsDocument(BaseModel):
             value (Any): The new value to assign to the specified attribute.
 
         Raises:
-            KeyError: If the attribute with the specified key does not exist in the document.
+            KeyError: If the attribute with the specified key does not exist 
+            in the document.
         """
         if hasattr(self, key):
             setattr(self, key, value)
