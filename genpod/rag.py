0c6f16e8a999abdbab3c2e87b508a1aa5a68feee
charan ravela <cravela@intelops.dev>
2024-12-03 01:03:41 -0600

    feat: updated code

--- genpod/rag.py
+++ genpod/rag.py
@@ -1,7 +1,6 @@
 from agents.rag_workflow.rag_graph import RAGWorkFlow
 from agents.rag_workflow.rag_state import RAGState
 from configs.project_config import ProjectAgents, ProjectGraphs
-from configs.supervisor_config import VECTOR_DB_COLLECTIONS
 from genpod.member import AgentMember
 
 
@@ -9,7 +8,7 @@ class RagMember(AgentMember[RAGState, RAGWorkFlow]):
     """
     """
 
-    def __init__(self, agents: ProjectAgents, graphs: ProjectGraphs, persistance_db_path: str, collection_name: str):
+    def __init__(self, agents: ProjectAgents, graphs: ProjectGraphs, persistance_db_path: str, collection_name: str, vector_db_path: str):
         """"""
 
         rag_config = agents.rag
@@ -25,6 +24,6 @@ class RagMember(AgentMember[RAGState, RAGWorkFlow]):
                 rag_config.llm, 
                 persistance_db_path, 
                 collection_name, 
-                VECTOR_DB_COLLECTIONS[collection_name]
+                vector_db_path
             )
         )
