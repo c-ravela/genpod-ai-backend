8a15716b1ed849fe847f0af5cf14830edea2a9ea
charan ravela <cravela@intelops.dev>
2025-02-13 12:54:43 -0600

    feat: update auto_repr

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,4 +1,7 @@
+import os
+from enum import Enum
 from functools import wraps
+from pprint import pformat
 
 from utils.logs.logging_utils import logger
 
@@ -49,27 +52,87 @@ def auto_init(init_func):
 
     return wrapper
 
-def auto_repr(cls):
+def _unwrap_enums(obj):
+    """
+    Recursively unwrap enum values from the given object.
+    
+    - If the object is an instance of an Enum, return its underlying value.
+    - If the object is an enum type (a subclass of Enum), return a list of its unwrapped members.
+    - Otherwise, process containers (lists, tuples, sets, dicts) recursively.
+    """
+    if isinstance(obj, Enum):
+        return unwrap_enums(obj.value)
+    elif isinstance(obj, type) and issubclass(obj, Enum):
+        return [unwrap_enums(member) for member in obj]
+    elif isinstance(obj, list):
+        return [unwrap_enums(item) for item in obj]
+    elif isinstance(obj, tuple):
+        return tuple(unwrap_enums(item) for item in obj)
+    elif isinstance(obj, set):
+        return {unwrap_enums(item) for item in obj}
+    elif isinstance(obj, dict):
+        return {key: unwrap_enums(value) for key, value in obj.items()}
+    return obj
+
+def auto_repr(_cls=None, *, include_private=False):
     """
     Class decorator that adds an auto-generated __repr__ method to a class.
-
+    
     The generated __repr__ method returns a string in the format:
         ClassName(field1=value1, field2=value2, ...)
     
-    Only attributes that do not start with an underscore (_) are included in the representation.
-    This is useful for debugging and logging.
-
-    Args:
-        cls (Type): The class to decorate.
-
-    Returns:
-        Type: The decorated class with an auto-generated __repr__ method.
-    """
-    def __repr__(self):
-        public_attrs = {key: value for key, value in self.__dict__.items() if not key.startswith('_')}
-        attr_str = ", ".join(f"{key}={value!r}" for key, value in public_attrs.items())
-        return f"{self.__class__.__name__}({attr_str})"
+    By default, only attributes that do not start with an underscore (_) are included.
+    You can change this behavior by setting `include_private=True` when applying the decorator.
     
-    cls.__repr__ = __repr__
-    logger.debug("Auto-generated __repr__ method added to class %s", cls.__name__)
-    return cls
+    The output format is configurable via environment variables:
+      - AUTO_REPR_PRETTY: If truthy, uses a pretty-formatted output (using pformat).
+      - AUTO_REPR_UNWRAP_ENUMS: If truthy, enum instances are replaced by their underlying values,
+        and enum types are replaced by a list of their unwrapped members.
+    
+    Args:
+        include_private (bool): Whether to include private fields (those starting with '_').
+                                Defaults to False.
+    
+    Returns:
+        The decorated class with an auto-generated __repr__ method.
+    
+    Usage:
+        @auto_repr
+        class MyClass:
+            ...
+        
+        # or to include private fields:
+        @auto_repr(include_private=True)
+        class MyClass:
+            ...
+    """
+    use_pretty = os.environ.get("AUTO_REPR_PRETTY", "False").lower() in ("true", "1", "yes")
+    unwrap_enums_flag = os.environ.get("AUTO_REPR_UNWRAP_ENUMS", "False").lower() in ("true", "1", "yes")
+
+    def decorator(cls):
+        def __repr__(self):
+            if include_private:
+                attrs = self.__dict__
+            else:
+                attrs = {key: value for key, value in self.__dict__.items() if not key.startswith('_')}
+            
+            if unwrap_enums_flag:
+                attrs = _unwrap_enums(attrs)
+            
+            if use_pretty:
+                formatted_attrs = pformat(attrs, indent=2, width=80, compact=True)
+                return f"{self.__class__.__name__}({formatted_attrs})"
+            else:
+                attr_str = ", ".join(f"{key}={value!r}" for key, value in attrs.items())
+                return f"{self.__class__.__name__}({attr_str})"
+        
+        cls.__repr__ = __repr__
+        logger.debug("Auto-generated __repr__ method added to class %s", cls.__name__)
+        return cls
+
+    if _cls is None:
+        # Decorator is used with parameters.
+        return decorator
+    else:
+        # Decorator is used without parameters.
+        return decorator(_cls)
