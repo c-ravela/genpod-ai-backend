6d18abc631e3ee5ce2fac77629427245f790b7f0
charan ravela <cravela@intelops.dev>
2025-05-08 12:20:04 -0500

    feat: update rag graph

--- agents/rag/_internal/rag_graph.py
+++ agents/rag/_internal/rag_graph.py
@@ -80,6 +80,7 @@ class RAGGraph(BaseGraph[RAGWorkFlow]):
             self.work_flow.router, 
             {
                 str(RAGNodeEnum.GRADE_DOCUMENTS): str(RAGNodeEnum.GRADE_DOCUMENTS),
+                str(RAGNodeEnum.RETRIEVE_DOCUMENTS): str(RAGNodeEnum.RETRIEVE_DOCUMENTS),
                 str(RAGNodeEnum.EXIT): str(RAGNodeEnum.EXIT)
             }
         )
@@ -89,6 +90,7 @@ class RAGGraph(BaseGraph[RAGWorkFlow]):
             str(RAGNodeEnum.GRADE_DOCUMENTS),
             self.work_flow.router, 
             {
+                str(RAGNodeEnum.GRADE_DOCUMENTS): str(RAGNodeEnum.GRADE_DOCUMENTS),
                 str(RAGNodeEnum.TRANSFORM_QUERY): str(RAGNodeEnum.TRANSFORM_QUERY),
                 str(RAGNodeEnum.GENERATE_RESPONSE): str(RAGNodeEnum.GENERATE_RESPONSE)
             }
@@ -99,6 +101,7 @@ class RAGGraph(BaseGraph[RAGWorkFlow]):
             str(RAGNodeEnum.TRANSFORM_QUERY),
             self.work_flow.router,
             {
+                str(RAGNodeEnum.TRANSFORM_QUERY): str(RAGNodeEnum.TRANSFORM_QUERY),
                 str(RAGNodeEnum.RETRIEVE_DOCUMENTS): str(RAGNodeEnum.RETRIEVE_DOCUMENTS),
                 str(RAGNodeEnum.EXIT): str(RAGNodeEnum.EXIT)
             }
@@ -109,6 +112,7 @@ class RAGGraph(BaseGraph[RAGWorkFlow]):
             str(RAGNodeEnum.GENERATE_RESPONSE),
             self.work_flow.router,
             {
+                str(RAGNodeEnum.GENERATE_RESPONSE): str(RAGNodeEnum.GENERATE_RESPONSE),
                 str(RAGNodeEnum.GRADE_RESPONSE): str(RAGNodeEnum.GRADE_RESPONSE),
                 str(RAGNodeEnum.EXIT): str(RAGNodeEnum.EXIT)
             }
@@ -119,6 +123,7 @@ class RAGGraph(BaseGraph[RAGWorkFlow]):
             str(RAGNodeEnum.GRADE_RESPONSE),
             self.work_flow.router,
             {
+                str(RAGNodeEnum.GRADE_RESPONSE): str(RAGNodeEnum.GRADE_RESPONSE),
                 str(RAGNodeEnum.GENERATE_RESPONSE): str(RAGNodeEnum.GENERATE_RESPONSE),
                 str(RAGNodeEnum.TRANSFORM_QUERY): str(RAGNodeEnum.TRANSFORM_QUERY),
                 str(RAGNodeEnum.EXIT): str(RAGNodeEnum.EXIT)
